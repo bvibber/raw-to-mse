e0e07c6e98a66c509f76141f80091351323450a7
Brion Vibber <brion@pobox.com>
2019-05-24 11:41:26 -0700

    use production mode, faster on edge
    macroblock copy specifically is hella slow on Edge in dev mode
    for some unknown reason.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 module.exports = {
   entry: './demo/demo.js',
-  mode: 'development',
+  //mode: 'development', // warning: macroblock copy ends up slow in dev mode on pre-Chromium Edge
+  mode: 'production',
   output: {
     filename: 'demo.js',
     path: path.resolve(__dirname, 'dist')
